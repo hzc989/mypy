bd311164572395de45218873abd663ad29cf8740
Houston Wong <hzc989@outlook.com>
2015-04-29 12:22:00 +0800

    latest modified

--- checkrt.py
+++ checkrt.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-#########################################################################
+"""
 # File Name: checkrt.py
 # Author: Houston Wong
 # mail: hzc989@163.com
 # Created Time: Tue 28 Apr 2015 05:28:45 PM HKT
-#########################################################################
+# Usage: checkrt.py ip|host
+"""
 
 from subprocess import Popen,PIPE
 import sys
@@ -25,8 +26,6 @@ logging.info('START checking route to %s' %ip)
 p = Popen('mtr -c 100 -r -n %s' %ip, shell = True, stdout = PIPE, stderr = PIPE)
 p.wait()
 stdoutdata, stderrdata = p.communicate()
-if(stderrdata):
-	logging.error(stderrdata)
 if(p.returncode == 0):
 	lastline = stdoutdata.splitlines().pop().split()
 	loss = float(lastline[2].replace('%',''))
@@ -36,3 +35,5 @@ if(p.returncode == 0):
 		logging.warn("here is result of the command mtr with 100 packets\n%s" %stdoutdata)
 	else:
 		logging.info('checking route to %s DONE' %ip)
+elif(stderrdata):
+	logging.error(stderrdata)
